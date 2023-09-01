b8a37f38404b3a2a563d66d8d689148b1647c395
SankojuRamesh <visitrameshs@gmail.com>
2023-09-01 15:26:03 +0530

    jwt authanication

--- sd/settings.py
+++ sd/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles', 
     'corsheaders',
      'rest_framework',
-     
      'django_filters',
     'drf_yasg',
     "menumanager",
@@ -54,19 +53,14 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE = [
-   
-   
+MIDDLEWARE = [ 
     
- 'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-   
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',   
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-  
+    'django.contrib.sessions.middleware.SessionMiddleware',  
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    
+    'django.contrib.auth.middleware.AuthenticationMiddleware',    
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
