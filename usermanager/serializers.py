36acb13337c56fafb2361c172fd10044228c9a14
SankojuRamesh <visitrameshs@gmail.com>
2024-12-24 18:08:26 +0530

    remove u in tettext)laxy

--- usermanager/serializers.py
+++ usermanager/serializers.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework_simplejwt import serializers as jwt_serializers
 from rest_framework_simplejwt.tokens import RefreshToken
